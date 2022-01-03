1894ed9640231229beb9869a8ae357777fc85547
Darnoker <73139334+Darnoker@users.noreply.github.com>
2022-01-03 23:06:13 +0100

    hash2

--- hash2/hash2.py
+++ hash2/hash2.py
@@ -1,5 +1,6 @@
 from re import split
 
+
 class hashStructure:
     def __init__(self, count, surname):
         self.count = count
@@ -11,7 +12,7 @@ def h(key, modulo):
 
 
 def H(key, i, modulo):
-    return (h(key,modulo) + i) % modulo
+    return (h(key, modulo) + i) % modulo
 
 
 def convertToNumber(text, constant) -> int:
@@ -24,33 +25,62 @@ def convertToNumber(text, constant) -> int:
     return numberFromString
 
 
+def insertHash(value, m, hashList, hashStruct):
+    for i in range(m):
+        val = H(value, i, m)
+        if hashList[val] is None:
+            hashList[val] = hashStruct
+            return True
+        else:
+            hashList[val].count += 1
+
+    return False
+
+
+def findHash(key, m, hashList, c):
+    count = 0
+    for i in range(m):
+        val = convertToNumber(key, c)
+        val = H(val, i, m)
+        count += 1
+        if hashList[val] is not None:
+            dupa = hashList[val].surname
+            if hashList[val].surname == key:
+                print("Liczba prób dla klucza:", key, ":", count)
+                return True
+
+    print("Liczba prób dla klucza:", key, ":", count)
+    return False
+
 
 # main
 
-m = 4
-hashList = [None]*m
+m = 2001
+c = 3
+size = 0
+eigthSize = int(m * 0.8)
+hashList = [None] * m
+surnameList = []
 with open("nazwiska.txt") as f:
     for row in f:
+        if size >= eigthSize:
+            break
         surname = row.split(" ")
         surname = surname[-1]
         surname = surname[:-1]
 
+        surnameList.append(surname)
+
         hashStruct = hashStructure(0, surname)
-        value = convertToNumber(surname, 3)
+        value = convertToNumber(surname, c)
 
-        for i in range(m):
-            val = H(value, i, m)
-            if hashList[val] is None:
-                hashList[val] = hashStruct
-                break
-            else:
-                hashList[val].count += 1
-
-for i in range(len(hashList)):
-    if hashList[val] is not None:
-        print(hashList[val].count)
+        if insertHash(value, m, hashList, hashStruct):
+            size += 1
 
 
+
+for surname in surnameList:
+    findHash(surname, m, hashList, c)
 
 
 
